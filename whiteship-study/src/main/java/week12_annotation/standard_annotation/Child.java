4960275e32eb60c4aabe550d32e301926b62e499
jjb8966 <jjb8966@naver.com>
2023-04-20 17:17:23 +0900

    12주차 어노테이션 복습
    - @Override

---
+++ whiteship-study/src/main/java/week12_annotation/standard_annotation/Child.java
@@ -0,0 +1,10 @@
+package week12_annotation.standard_annotation;
+
+public class Child extends Parent {
+
+//     리턴 타입 또는 메소드 시그니처가 다른 경우 컴파일 에러
+//    @Override
+//    public int method() {
+//        super.method();
+//    }
+}
---
+++ whiteship-study/src/main/java/week12_annotation/standard_annotation/Parent.java
@@ -0,0 +1,8 @@
+package week12_annotation.standard_annotation;
+
+public class Parent {
+
+    public void method() {
+
+    }
+}
